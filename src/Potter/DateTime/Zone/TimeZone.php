86ce4cf2e4b8af4e970ea7f87fa4a4c793c546d7
Jay Potter <j@ypotter.ca>
2024-11-02 15:56:47 -0400

    Update TimeZone.php

--- src/Potter/DateTime/Zone/TimeZone.php
+++ src/Potter/DateTime/Zone/TimeZone.php
@@ -7,10 +7,8 @@ namespace Potter\DateTime\Zone;
 use DateTimeZone;
 
 final class TimeZone extends AbstractTimeZone
-{ 
-    use StringableTrait;
-    
-    public static function inherit(DateTimeZone $timeZone): static
+{    
+    public static function inherit( DateTimeZone $timeZone): static
     {
         return new self($timeZone->getName());
     }
