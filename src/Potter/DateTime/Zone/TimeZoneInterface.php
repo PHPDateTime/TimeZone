4c87955274cc3f8f04404afe5e06cc5ef79990c7
Jay Potter <j@ypotter.ca>
2024-11-02 12:45:00 -0400

    Create TimeZoneInterface.php

---
+++ src/Potter/DateTime/Zone/TimeZoneInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\DateTime\Zone;
+
+use DateTimeInterface as DateTimeInterface,
+    DateTimeZone      as DateTimeZone;
+
+use Potter\Stringable\StringableInterface;
+
+interface TimeZoneInterface extends StringableInterface
+{
+    public function getLocation(): array|false;
+    public function getName(): string;
+    public function getOffset(DateTimeInterface $datetime): int;
+    public function getTransitions(int $timestampBegin = PHP_INT_MIN, int $timestampEnd = PHP_INT_MAX): array|false;
+    
+    public static function listAbbreviations(): array;
+    public static function listIdentifiers(int $timezoneGroup = DateTimeZone::ALL, ?string $countryCode = null): array;
+    
+    public static function inherit(DateTimeZone $timeZone): static;
+}
